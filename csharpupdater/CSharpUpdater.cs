8022016e545fd5977f92976a516af58c2ac6ad4d
Lucas Meijer <lucas@unity3d.com>
2013-10-29 13:35:34 -0700

    first step towards updating many scripts at the same time, to be able to resolve more types accurately

--- csharpupdater/CSharpUpdater.cs
+++ csharpupdater/CSharpUpdater.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using ICSharpCode.NRefactory.CSharp;
 using ICSharpCode.NRefactory.CSharp.Resolver;
+using ICSharpCode.NRefactory.CSharp.TypeSystem;
 using ICSharpCode.NRefactory.Editor;
 using ICSharpCode.NRefactory.TypeSystem;
 using ScriptUpdating;
+using CSharpParser = ICSharpCode.NRefactory.CSharp.CSharpParser;
 
 namespace CSharpUpdater
 {
@@ -20,31 +23,73 @@ namespace CSharpUpdater
 			return Update(input, SmallPipeline);
 		}
 
+		class SourceFileData
+		{
+			public SyntaxTree SyntaxTree;
+			public CSharpUnresolvedFile UnresolvedFile;
+			public ReadOnlyDocument Document;
+			public SourceFile SourceFile;
+		}
+
 		internal string Update(string input, Func<ReplacementCollector, CSharpAstResolver,IEnumerable<ReplacingAstVisitor>> pipeLineProvider)
 		{
-			var parser = new CSharpParser();
-			var doc = new ReadOnlyDocument(input);
-			var syntaxTree = parser.Parse(doc,"myfile.cs");
-		
 			IProjectContent project = new CSharpProjectContent();
 			var cecilLoader = new CecilLoader { LazyLoad = true };
 			var assembly = cecilLoader.LoadAssemblyFile("C:/Program Files (x86)/Unity/Editor/Data/PlaybackEngines/windowsstandaloneplayer/Managed/UnityEngine.dll");
 			project = project.AddAssemblyReferences(assembly);
+			project.AddAssemblyReferences(new CecilLoader().LoadAssemblyFile(typeof(object).Assembly.Location));
 
-			project.AddAssemblyReferences(new CecilLoader().LoadAssemblyFile(typeof (object).Assembly.Location));
-			var unresolvedFile = syntaxTree.ToTypeSystem();
-			project = project.AddOrUpdateFiles(unresolvedFile);
+			var sourceFilesData = SourceFilesDataFor(input);
 
-			var resolver = new CSharpAstResolver(project.CreateCompilation(), syntaxTree, unresolvedFile);
-			var replacementCollector = new ReplacementCollector();
+			project = AddSourceFilesToProject(sourceFilesData, project);
 
 			if (pipeLineProvider == null)
 				pipeLineProvider = BuildPipeline;
 
-			foreach (var visitor in pipeLineProvider(replacementCollector, resolver))
-				syntaxTree.AcceptVisitor(visitor);
+			var compilation = project.CreateCompilation();
 
-			return replacementCollector.ApplyTo(doc);
+			foreach (var sourceFileData in sourceFilesData)
+			{
+				var resolver = new CSharpAstResolver(compilation, sourceFileData.SyntaxTree, sourceFileData.UnresolvedFile);
+				var replacementCollector = new ReplacementCollector();
+
+				foreach (var visitor in pipeLineProvider(replacementCollector, resolver))
+					sourceFileData.SyntaxTree.AcceptVisitor(visitor);
+
+				sourceFileData.SourceFile.Contents = replacementCollector.ApplyTo(sourceFileData.Document);
+			}
+
+			return sourceFilesData.Single().SourceFile.Contents;
+		}
+
+		private static IProjectContent AddSourceFilesToProject(IEnumerable<SourceFileData> sourceFilesData, IProjectContent project)
+		{
+			foreach (var sourceFileData in sourceFilesData)
+				project = project.AddOrUpdateFiles((sourceFileData.UnresolvedFile));
+
+			return project;
+		}
+
+		private static List<SourceFileData> SourceFilesDataFor(string input)
+		{
+			var sourceFilesData = new List<SourceFileData>();
+
+			var sourceFiles = new List<SourceFile>() {new SourceFile() {Contents = input, FileName = "bla.cs"}};
+
+			foreach (var sourceFile in sourceFiles)
+			{
+				var doc = new ReadOnlyDocument(input);
+				var syntaxTree = new CSharpParser().Parse(doc, sourceFile.FileName);
+				var unresolvedFile = syntaxTree.ToTypeSystem();
+				sourceFilesData.Add(new SourceFileData()
+				                    {
+					                    Document = doc,
+					                    SyntaxTree = syntaxTree,
+					                    UnresolvedFile = unresolvedFile,
+					                    SourceFile = sourceFile
+				                    });
+			}
+			return sourceFilesData;
 		}
 
 		private IEnumerable<ReplacingAstVisitor> BuildPipeline(ReplacementCollector replacementCollector, CSharpAstResolver resolver)
